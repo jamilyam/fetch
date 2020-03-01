feddf8bb761d57b5b7fcb0905a006a3688f9fde3
jamilyam <45009272+jamilyam@users.noreply.github.com>
2020-03-01 23:52:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,28 +12,3 @@ function getRequest() {
     document.getElementById('result').innerHTML = output;
   })
 }
-
-
-function onSearch() {
-    let input = getElementById('form-value').value;
-
-
-request = new XMLHttpRequest;
-	request.open('GET', 'http://api.giphy.com/v1/gifs/search?q&api_key=DN35CTOvDRXCi1IOVuVsOdRlBXwoFZLU'+input, true);
-	
-	request.onload = function() {
-		if (request.status >= 200 && request.status < 400){
-			data = JSON.parse(request.responseText).data.image_url;
-			console.log(data);
-			document.getElementById("here_is_gif").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
-		} else {
-			console.log('reached giphy, but API returned an error');
-		 }
-	};
-
-	request.onerror = function() {
-		console.log('connection error');
-	};
-
-  request.send();
-}  
